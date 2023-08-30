cc992ad6a044e5b32a42bc9c7d8de353f862bc0a
kingRdelgado <142425309+kingRdelgado@users.noreply.github.com>
2023-08-30 10:57:52 -0500

    Add files via upload

---
+++ src/call/unicauca/edu/co/ex_atrr.java
@@ -0,0 +1,9 @@
+
+package call.unicauca.edu.co;
+
+
+public class ex_atrr {
+     public String name = " Ricardo Delgado Urbano";  
+    private  String document = "105936473";
+     public int year_hbd = 2006; 
+}
---
+++ src/exercise_class/Exercise_class.java
@@ -0,0 +1,19 @@
+
+package exercise_class;
+import call.unicauca.edu.co.ex_atrr;
+        
+        
+public class Exercise_class {
+
+    public static void main(String[] args) {
+     
+        ex_atrr atrr = new ex_atrr();
+        System.out.println(" Access var: ");        
+                System.out.println(" Import name I1: "+ atrr.name) ; 
+                atrr.name="Camilo";
+                System.out.println(" Import name I2: "+ atrr.name ) ; 
+                 System.out.println(" Import name: "+ atrr.year_hbd ) ; 
+                
+    }
+    
+}

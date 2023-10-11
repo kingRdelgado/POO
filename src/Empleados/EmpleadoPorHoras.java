95adfe450edd62b650922d5687727f112b71c8bf
kingRdelgado <142425309+kingRdelgado@users.noreply.github.com>
2023-10-11 16:39:50 -0600

    Update EmpleadoPorHoras.java

--- src/Empleados/EmpleadoPorHoras.java
+++ src/Empleados/EmpleadoPorHoras.java
@@ -1,37 +1,36 @@
 
+///
 package Empleados;
 
-
 public class EmpleadoPorHoras extends Empleado{
-     private int horasTrabajadas;
-    private double tarifaPorHora; 
+    private int horasTrabajadas;
+  private double tarifaPorHora;
 
-    public EmpleadoPorHoras(int horasTrabajadas, double tarifaPorHora, String nombre, double salarioBase) {
-        super(nombre, salarioBase);
-        this.horasTrabajadas = horasTrabajadas;
-        this.tarifaPorHora = tarifaPorHora;
-    }
+  public EmpleadoPorHoras(String nombre, int horasTrabajadas, double tarifaPorHora) {
+    super(nombre, 0);
+    this.horasTrabajadas = horasTrabajadas;
+    this.tarifaPorHora = tarifaPorHora;
+  }
 
-    public int getHorasTrabajadas() {
-        return horasTrabajadas;
-    }
+  public int getHorasTrabajadas() {
+    return this.horasTrabajadas;
+  }
 
-    public void setHorasTrabajadas(int horasTrabajadas) {
-        this.horasTrabajadas = horasTrabajadas;
-    }
+  public void setHorasTrabajadas(int horasTrabajadas) {
+    this.horasTrabajadas = horasTrabajadas;
+  }
 
-    public double getTarifaPorHora() {
-        return tarifaPorHora;
-    }
+  public double getTarifaPorHora() {
+    return this.tarifaPorHora;
+  }
 
-    public void setTarifaPorHora(double tarifaPorHora) {
-        this.tarifaPorHora = tarifaPorHora;
-    }
-  
-    
-    @Override
-    public double calcularsalario(){
-     return horasTrabajadas*tarifaPorHora;
-    }
+  public void setTarifaPorHora(double tarifaPorHora) {
+    this.tarifaPorHora = tarifaPorHora;
+  }
+
+  @Override
+  public double calcularSalario() {
+    return  getTarifaPorHora()*getHorasTrabajadas();
+  }
     
 }

1666c069be02717dd5877098556af5e59e4cba8a
kingRdelgado <142425309+kingRdelgado@users.noreply.github.com>
2023-10-11 16:37:53 -0600

    Update Empleado.java

--- src/Empleados/Empleado.java
+++ src/Empleados/Empleado.java
@@ -1,35 +1,21 @@
+
 package Empleados;
-import java.util.ArrayList;
-import java.util.List;
+public abstract class Empleado {
+  private String nombre;
+  private double salarioBase;
 
-abstract class Empleado {
-   private String nombre;
-   private double salarioBase;
+  public Empleado(String nombre, double salarioBase) {
+    this.nombre = nombre;
+    this.salarioBase = salarioBase;
+  }
 
-    public Empleado(String nombre, double salarioBase) {
-        this.nombre = nombre;
-        this.salarioBase = salarioBase;
-    }
+  public String getNombre() {
+    return this.nombre;
+  }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setSalarioBase(double salarioBase) {
-        this.salarioBase = salarioBase;
-    }
-    
-    
-    
-   
-   public abstract double calcularsalario();
-   public String getNombre(){
-   return nombre;
-           }
-public double getSalarioBase(){
-return salarioBase;}
-       
-        
-   
-    
+  public double getSalarioBase() {
+    return this.salarioBase;
+  }
+  public abstract double calcularSalario();
 }
+

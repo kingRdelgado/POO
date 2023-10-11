6c216dcd8c1e8f5894b31bc8ce9433d6815f3bcc
kingRdelgado <142425309+kingRdelgado@users.noreply.github.com>
2023-10-11 16:38:40 -0600

    Update EmpleadoAsalariado.java

--- src/Empleados/EmpleadoAsalariado.java
+++ src/Empleados/EmpleadoAsalariado.java
@@ -1,17 +1,28 @@
 
+//
 package Empleados;
 
-public class EmpleadoAsalariado extends Empleado {
- private double deducciones; 
+public class EmpleadoAsalariado extends Empleado{
+  private double deducciones;
 
-    public EmpleadoAsalariado(String nombre, int par, int salarioBase) {
-        super(nombre, salarioBase);
-        this.deducciones = deducciones;
+  public EmpleadoAsalariado(String nombre, double salarioBase, double deducciones) {
+    super(nombre, salarioBase);
+    this.deducciones = deducciones;
+  }
+
+  public double getDeducciones() {
+    return this.deducciones;
+  }
+
+  public void setDeducciones(double deducciones) {
+    this.deducciones = deducciones;
+  }
+
+  @Override
+  public double calcularSalario() {
+    return getSalarioBase() - getDeducciones();
+  }
+    public void getDeducciones(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
- 
-    @Override
-    public double calcularsalario() {
-        return getSalarioBase()-deducciones;
-    }
- 
-          }
+}
